03a548896e57c90749ef3b6e66db77a64b8277c3
Tamar Nasrashvili <tamari.nasrashvili0@outlook.com>
2020-05-07 22:54:03 +0400

    add project

---
+++ TSUViewEngine/ViewComponent/SliderViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TSUViewEngine.ViewComponent
+{
+    public class SliderViewComponent: Microsoft.AspNetCore.Mvc.ViewComponent
+    {
+        private Models.MovieRepository.IMovieRepository _movieRepository;
+
+        public SliderViewComponent(Models.MovieRepository.IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            IEnumerable<string> movies = _movieRepository.Movies("").Select(x => x.Thumb);
+
+            return View(movies);
+        }
+    }
+}
